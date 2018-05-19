0da02e8188b24857ec9144914413ec58cf5b660b
HossamHassan47 <hossam.hassan@outlook.com>
2018-05-19 02:16:36 +0200

    quiz activity

---
+++ app/src/main/java/com/wordpress/hossamhassan47/quiz/model/Question.java
@@ -0,0 +1,11 @@
+package com.wordpress.hossamhassan47.quiz.model;
+
+public class Question {
+    public String Id;
+    public String Title;
+    public String Option1;
+    public String Option2;
+    public String Option3;
+    public String Option4;
+    public String CorrectAnswer;
+}
